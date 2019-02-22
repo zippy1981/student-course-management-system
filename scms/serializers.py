8055d0e92aea424b2e9ede9f5e29279d238bd368
Justin Dearing <zippy1981@gmail.com>
2019-02-22 12:49:13 -0500

    Add some logging to figure out the course scheduler.

--- scms/serializers.py
+++ scms/serializers.py
@@ -1,18 +1,53 @@
+import logging
 from rest_framework import serializers
 from scms import models
 
+logger = logging.getLogger('zippysoft.scms.serializers')
+logger.debug(f'In Serializers for {__name__}')
 
-class StudentSerializer(serializers.HyperlinkedModelSerializer):
+class LoggingModelSerializer(serializers.ModelSerializer):
+    """
+    ModelSerializer with debug logging
+    """
+
+    def create(self, validated_data):
+        logger.debug(f'Creating with {validated_data}')
+        return super().create(validated_data)
+
+    def get_attribute(self, instance):
+        logger.debug(f'Getting Attribute {instance}')
+        return super().get_attribute(instance)
+
+    def is_valid(self, raise_exception=False):
+        logger.debug(f'Checking if data is valid in {self.__class__.__name__}. Thow: {raise_exception}')
+        return super().is_valid(raise_exception=raise_exception)
+
+    def save(self, **kwargs):
+        logger.debug(f'{self.__class__.__name__}.save() with args [{kwargs}]')
+        return super().save(**kwargs)
+
+    def to_representation(self, instance):
+        logger.debug(f'Calling {self.__class__.__name__}.to_representation() with {instance}')
+        if hasattr(instance, 'department'):
+            logger.debug(f'Department of instance is {instance.department}')
+        return super().to_representation(instance)
+
+    def to_internal_value(self, data):
+        logger.debug(f'Calling {self.__class__.__name__}.to_internal_value() with {data}')
+        return super().to_internal_value(data)
+
+
+class StudentSerializer(LoggingModelSerializer):
     class Meta:
         model = models.Student
         fields = ('id', 'last_name', 'first_name', 'full_name')
 
-class DepartmentSerializer(serializers.HyperlinkedModelSerializer):
+class DepartmentSerializer(LoggingModelSerializer):
     class Meta:
         model = models.Department
-        #fields = ('id', 'last_name', 'first_name')
+        fields = ('name',)
 
-class CourseSerializer(serializers.ModelSerializer):
+class CourseSerializer(LoggingModelSerializer):
     department = serializers.SlugRelatedField(
         many = False,
         read_only = False,
@@ -29,24 +64,40 @@ class CourseSerializer(serializers.ModelSerializer):
             'course_number', 
             'credits')
 
-class CourseInstanceSerializer(serializers.ModelSerializer):
+class CourseSummarySerializer(LoggingModelSerializer):
+    """
+    Serializer for course summaries as a child object
+    """
     department = serializers.SlugRelatedField(
         many = False,
-        read_only = True,
-        slug_field = 'name'
-    )
-    course = serializers.SlugRelatedField(
-        many = False,
         read_only = False,
-        slug_field = 'course_number',
-        queryset = models.Course.objects.all()
-    )
+        slug_field = 'name',
+        queryset = models.Department.objects.all()
+     )
+
+    class Meta:
+        model = models.Course
+        fields = (
+            'department',
+            'course_number')
+
+class CourseInstanceSerializer(LoggingModelSerializer):
+    course = CourseSummarySerializer()
+    
+    def create(self, validated_data):
+        pushed_course = validated_data.pop('course')
+        logger.debug(f'CourseInstanceSerializer.create() looking up course info for {pushed_course}')
+        department = models.Department.objects.get(name= pushed_course.department)
+        course_model = models.Course.get(course_number=pushed_course.course_number, department = department.id)
+        validated_data.course = course_model.id
+        logger.debug('CourseInstanceSerializer.create() has modified the request json')
+        return super().create(validated_data)
+
 
     class Meta:
         model = models.CourseInstance
         fields = (
             'id', 
-            'department',
             'course', 
             'start_date',
             'end_date',
--- scms/views.py
+++ scms/views.py
@@ -1,14 +1,41 @@
+import logging
 from rest_framework import viewsets
 from scms import models, serializers
 
-class StudentViewSet(viewsets.ModelViewSet):
+logger = logging.getLogger('zippysoft.scms.views')
+logger.debug(f'In Views for {__name__}')
+
+class LoggingModelViewSet(viewsets.ModelViewSet):
+    """
+    ModelViewSet with debug logging
+    """
+
+    def create(self, request, *args, **kwargs):
+        logger.debug(f'In create for {self.__class__.__name__}. {request.path}')
+        return super().create(request, *args, **kwargs)
+
+    def get_serializer(self, *args, **kwargs):
+        logger.debug(f'Getting serializer for {self.__class__.__name__} ({args}) [{kwargs}]')
+        s = super().get_serializer(*args, **kwargs)
+        return s
+
+    def perform_create(self, serializer):
+        logger.debug(f'In perform_create with {serializer}')
+        return super().perform_create(serializer)
+
+    def list(self, request, *args, **kwargs):
+        logger.debug(f'In list for {__name__}. {request.path}')
+        return super().list(request, *args, **kwargs)
+
+
+class StudentViewSet(LoggingModelViewSet):
     """
     API endpoint that allows students to be viewed or edited.
     """
     queryset = models.Student.objects.all().order_by('last_name','first_name')
     serializer_class = serializers.StudentSerializer
 
-class CourseViewSet(viewsets.ModelViewSet):
+class CourseViewSet(LoggingModelViewSet):
     """
     API endpoint that allows courses to be viewed or edited.
     """
@@ -16,9 +43,12 @@ class CourseViewSet(viewsets.ModelViewSet):
     serializer_class = serializers.CourseSerializer
 
 
-class CourseInstanceViewSet(viewsets.ModelViewSet):
+class CourseInstanceViewSet(LoggingModelViewSet):
     """
     API endpoint that allows instances of courses to be viewed or edited.
     """
     queryset = models.CourseInstance.objects.all().order_by('course__department__name','course__course_number', 'start_date')
     serializer_class = serializers.CourseInstanceSerializer
+
+    def create(self, request, *args, **kwargs):
+        return super().create(request, *args, **kwargs)
