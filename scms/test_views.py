c1edadee8ffa1e5f8b5c7bb4057b805d82afe5a9
Justin Dearing <zippy1981@gmail.com>
2019-06-03 09:47:50 -0400

    WIP before I delete this locally

--- scms/test_views.py
+++ scms/test_views.py
@@ -1,20 +1,23 @@
-from django.test import TestCase
 from scms import models, views
+from rest_framework.test import APIRequestFactory, APITestCase
 from unittest import skip
 
-class StudentTestCase(TestCase):
+class StudentTestCase(APITestCase):
 
     def setUp(self):
-        self.studentView = views.StudentViewSet(action='get', kwargs={})
+        self.request_factory = APIRequestFactory()
+        self.student_view = views.StudentViewSet(action='get', kwargs={})
         models.Student.objects.create(first_name="Justin", last_name="Dearing")
         models.Student.objects.create(first_name="Bob", last_name="Smith")
         models.Student.objects.create(first_name="Tom", last_name="Smith")
 
-    @skip("Need to do a lot of mocking to get this working.")
+    #@skip("Need to do a lot of mocking to get this working.")
     def test_get_students(self):
-        self.assertEqual('Student', self.studentView.get_object())
+        response = self.request_factory.get('api/students/?format=json')
+        self.assertEqual(200, response.status_code)
+        self.assertEqual(3, len(response.body))
 
     def test_get_metadata(self):
-        self.assertEqual('get', self.studentView.action)
-        self.assertEqual('Student', self.studentView.get_view_name())
-        self.assertEqual('API endpoint that allows students to be viewed or edited.', self.studentView.get_view_description())
+        self.assertEqual('get', self.student_view.action)
+        self.assertEqual('Student', self.student_view.get_view_name())
+        self.assertEqual('API endpoint that allows students to be viewed or edited.', self.student_view.get_view_description())
